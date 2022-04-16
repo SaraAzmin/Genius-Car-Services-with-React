7a1e45388cc9193a0b8e62a86ede95329a4ebdad
SaraAzmin <azmin.sara17@gmail.com>
2022-04-16 19:33:52 +0200

    login and register form added with navigation

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+
+    const navigateRegister = () => {
+        navigate('/register');
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(email, password)
+    }
+
     return (
-        <div>
-            <h2>Please Login</h2>
+        <div className='container w-50 mx-auto mt-5'>
+            <h2 className='text-center text-primary mb-5'>Please Login</h2>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            <p>New here? <Link to='/register' className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link></p>
         </div>
     );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 
 const Register = () => {
+
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
     return (
-        <div>
-            <h2>Register Here</h2>
+        <div className='register-form'>
+            <h2 className='text-center text-primary my-5'>Register Here</h2>
+            <form >
+                <input type="text" name="name" id="" placeholder='Your Name' />
+                <br></br>
+                <input type="email" name="email" id="" placeholder='Your Email' required />
+                <br></br>
+                <input type="password" name="password" id="" placeholder='Your Password' required />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
         </div>
     );
 };
